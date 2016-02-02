c566f481cb3316e55d45b08925b48877de21e1f8
Sean Conaty <anxiolydiot@nbp-60-239.nbp.ruw.rutgers.edu>
2016-02-01 20:06:46 -0500

    lost track of variable names, fixed what wasn't corretly defined

--- server.js
+++ server.js
@@ -1,44 +1,29 @@
 var http = require("http");
 
-
 var PORT = 7000;
 var PORTbad = 7500;
 var bad = ['u suck','go away'];
 var good = ['ur nice','stay here'];
-var anyGood = [Math.floor(Math.random()* anygood.length)];
-var anyBad = [Math.floor(Math.random()* anybad.length)];
+var anyGood = [Math.floor(Math.random()* good.length)];
+var anyBad = [Math.floor(Math.random()* bad.length)];
 
 var goodRequest = function(res, req){
   //res.write('u r nice');
-  res.write(anygood + req.url);
+  res.write(anyGood + req.url);
+}
 
 var badRequest = function(res, req){
   
-  res.write(anybad + req.url);
+  res.write(anyBad + req.url);
+}
 
-var goodServer = http.createServer(handleRequestGood);
-var badServer = http.createServer(handleRequestBad);
+var goodServer = http.createServer(goodRequest);
+var badServer = http.createServer(badRequest);
 
-  server.listen(PORT, function(){
+  goodServer.listen(PORT, function(){
     console.log('listening at http://localhost:%s', PORT);
 
   })
-};
-
-server.listen()
-
-
-// var PORT = 8080;
-  
-
-// var handleRequest = function(req, res){
-//   //res.write()
-//     res.end('hit:' + req.url);
-
-
-// var server = http.createServer(handleRequest);
-
-// server.listen(PORT, function(){
-//   console.log('listening at http://localhost:%s', PORT);
-
-// });
+  badServer.listen(PORTbad, function (){
+    console.log('listening at http://localhost:%s', PORTbad)
+  });
